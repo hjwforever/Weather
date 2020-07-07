acfacf21109995ca418a015f3f80c5dbc6f3e1e1
deng-shuo <ddll841022@qq.com>
2020-07-07 15:59:44 +0800

    邓硕在首页将未来天气连接到了数据库。

--- app/admin.py
+++ app/admin.py
@@ -5,9 +5,17 @@ from app.models import *
 
 
 # admin.site.register(models.User)
+class PredictDataAdmin(admin.ModelAdmin):
+    list_display = ('date', 'tmin', 'tmax', 'tavg', 'prcp')
+
+
+class HistoryDateAdmin(admin.ModelAdmin):
+    list_display = ('date', 'tmin', 'tmax', 'tavg', 'prcp')
+
+
 admin.site.register(User)
 admin.site.register(ConfirmString)
-admin.site.register(HistoryData)
-admin.site.register(PredictData)
+admin.site.register(HistoryData, HistoryDateAdmin)
+admin.site.register(PredictData, PredictDataAdmin)
 admin.site.register(Weather)
 
