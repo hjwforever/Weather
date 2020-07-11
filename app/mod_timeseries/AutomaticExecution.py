ad022b2d2e5bb5ba14eee3e544673196d8c2ce72
deng-shuo <ddll841022@qq.com>
2020-07-11 11:14:51 +0800

    邓硕修改了历史天气界面点击年份将日期月份下拉框清空

--- app/mod_timeseries/AutomaticExecution.py
+++ app/mod_timeseries/AutomaticExecution.py
@@ -76,7 +76,7 @@ def tick():
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
-    scheduler.add_job(tick, 'cron', hour=8, minute=55)
+    scheduler.add_job(tick, 'cron', hour=0, minute=1)
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C    '))
 
     try:
